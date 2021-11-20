9809699a586d01f95d85cc17a5b6cd088316ccf4
Anatoliy <anartius@gmail.com>
2021-11-20 17:58:24 +0100

    Finish of stage 1

--- Minesweeper/task/src/minesweeper/Main.kt
+++ Minesweeper/task/src/minesweeper/Main.kt
@@ -1,5 +1,13 @@
 package minesweeper
 
-fun main() {
+import kotlin.random.Random
 
+fun main() {
+    val field = List(9) { MutableList(9) {'.'} }
+    var minesPosition: Int
+    for (i in field.indices) {
+        minesPosition = Random.nextInt(0, 9)
+        field[i][minesPosition] = 'X'
+    }
+    field.forEach { println(it.joinToString("")) }
 }
