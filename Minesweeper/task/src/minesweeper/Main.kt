a3fa39fe8d282147c691fb3558c3b264d42889b4
Anatoliy <anartius@gmail.com>
2021-11-22 14:36:56 +0100

    Finish of stage 3

--- Minesweeper/task/src/minesweeper/Main.kt
+++ Minesweeper/task/src/minesweeper/Main.kt
@@ -17,9 +17,44 @@ fun main() {
         }
     }
 
+    // Checking how many mines there is around every free spot
+    for (x in field.indices) {
+        for (y in field[x].indices) {
+            if (field[x][y] == '.') {
+                var amountOfMinesAround = 0
+                for (i in 0..2) {
+                    try {
+                        if (field[x - 1][y - 1 + i] == 'X') amountOfMinesAround++
+                    } catch (e: IndexOutOfBoundsException) {
+                        continue
+                    }
+                }
+
+                if (y < field[x].lastIndex && field[x][y + 1] == 'X') {
+                    amountOfMinesAround++
+                }
+
+                for (j in 0..2) {
+                    try {
+                        if (field[x + 1][y - 1 + j] == 'X') amountOfMinesAround++
+                    } catch (e: IndexOutOfBoundsException) {
+                        continue
+                    }
+                }
+
+                if (y > 0 && field[x][y - 1] == 'X') {
+                    amountOfMinesAround++
+                }
+
+                if (amountOfMinesAround > 0) field[x][y] = amountOfMinesAround.toString().last()
+            }
+        }
+    }
+
     field.forEach { println(it.joinToString("")) }
 }
 
+// Input amount of mines on the field and return it: Unit -> Int
 fun getNumberOfMines() : Int {
     var amount: Int
     while (true) {
